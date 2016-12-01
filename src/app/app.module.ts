d9df7176d4b3bb46c4fe1d32fef7e497afe4e381
parneets <parneet.995@gmail.com>
2016-12-01 00:08:20 -0500

    Added basic authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/pages';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'e28e54b8'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -9,7 +16,8 @@ import { LoginPage } from '../pages/pages';
     LoginPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
 
 @Component({
   selector: 'login-page',
@@ -8,10 +9,61 @@ import { NavController } from 'ionic-angular';
 
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {}
+  details: UserDetails = {'email': '', 'password': ''};
+
+  constructor(public navCtrl: NavController,
+              public auth: Auth,
+              public user: User) { }
 
   ionViewDidLoad() {
     console.log('Hello LoginPage Page');
   }
+
+  // handles signIn button click
+  signIn(){
+
+    this.auth.login('basic', this.details).then(()=>{
+      console.log('Sign in successful');
+    }, (err)=> {
+      // unsuccessful
+      alert('Invalid username or password');
+    });
+  }
+
+  // handles signUp button click
+  signUp(){
+
+    this.auth.signup(this.details).then(() => {
+      // `this.user` is now registered
+      return this.auth.login('basic', this.details );
+
+    }, (err: IDetailedError<string[]>) => {
+      for (let e of err.details) {
+        switch (e) {
+          case 'required_email':
+            alert('Missing email field.');
+            break;
+
+          case 'required_password':
+            alert("Missing password field.");
+            break;
+          
+          case 'conflict_email':
+            alert("A user has already signed up with the supplied email.");
+            break;
+
+          case 'conflict_username':
+            alert("A user has already signed up with the supplied username.");
+            break;
+
+          case 'invalid_email':
+            alert("The email did not pass validation.");
+            break;
+        }
+      }
+    });
+  }
+
+
 
 }
